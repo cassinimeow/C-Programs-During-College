119738eca2bfdb5fb733cf8190c89159c46168cf
rycue <151157051+rycue@users.noreply.github.com>
2024-11-26 22:19:45 +0800

    Add while loop and gitignore

---
+++ WhileLoop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int counter = 1;
+
+    while (counter <= 5) {
+        printf("Counter is at: %d\n", counter);
+        counter++;
+    }
+
+    printf("Loop finished!\n");
+
+    return 0;
+}
