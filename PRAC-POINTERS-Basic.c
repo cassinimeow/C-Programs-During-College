142870fc20103352e1f7673092d7327ccfaa582c
rycue <151157051+rycue@users.noreply.github.com>
2024-12-10 09:10:04 +0800

    Add pointer code

--- PRAC-POINTERS-Basic.c
+++ PRAC-POINTERS-Basic.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
 int main() {
+
     int number = 33;
+    printf("Variable Value: %d\n", number);
+    printf("Memory Address in HEX: %p\n", &number);
+
+    // Using a pointer to store the address returned by the address operator and then dereferencing it.
+    int x = 10;
+    int* ptrX;
+    ptrX = &x;
+
+    printf("\nValue of accessed variable: %d\n", *ptrX);
     
     return 0;
 }
