14bc82e128ecdf203223928c78c0c7054174f3b3
Elizander Aguila <elizanderaguila@gmail.com>
2025-02-03 09:10:55 +0800

    meowers

--- xian.c
+++ xian.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    int grade[3];
-    int j;
+    char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    printf("%d", strlen(alphabet));
 
-    for (j = 0; j <= 2; j++)
-    {
-        printf("Enter Grades: ");
-        scanf("%d", &grade[j]);
-    }
-    
     return 0;
 }
